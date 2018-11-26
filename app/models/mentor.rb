6c1bfadc9a9814556aee43c31fee6f1c57ccfca5
Jay Vajanopath <jaycv@bu.edu>
2018-11-26 00:51:18 -0800

    did routes for 2 users and views and controllers and validation

---
+++ app/models/mentor.rb
@@ -0,0 +1,11 @@
+class Mentor < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nationality, presence: true
+  validates :university, presence: true
+  validates :degree_level, presence: true
+end
---
+++ app/models/prospective.rb
@@ -0,0 +1,9 @@
+class Prospective < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nationality, presence: true
+end
