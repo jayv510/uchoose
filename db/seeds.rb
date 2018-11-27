ec476c3c528a764a5dd4c59040b832f55a213d69
Stuart Nixon <stuartnixon359@gmail.com>
2018-11-27 13:53:37 +0800

    Created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting any existing users ..."
+Prospective.delete_all
+Mentor.delete_all
+
+
+start_id = User.last.id - User.count + 1
+end_id = User.last.id
+user_id_sample = *(start_id..end_id)
+
+majors = ["Agriculture & Natural Resources","Arts","  Biology & Life Science","Business","Communications & Journalism","Computers & Mathematics","Education","Engineering","Health","Humanities & Liberal Arts","Industrial Arts & Consumer Services","Interdisciplinary","Law & Public Policy","Physical Sciences","Psychology & Social Work","Social Science"]
+degree_levels = ["Undergraduate", "Honours", "Masters", "Doctoral", "Post-doctoral", "Other"]
+
+
+puts "Generating fake mentors ..."
+
+20.times do
+  Mentor.create(
+    email: Faker::Internet.email,
+    password: "123456",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    nationality: Faker::Nation.nationality,
+    university: Faker::University.name,
+    major: majors.sample,
+    degree_level: degree_levels.sample,
+    description: Faker::Lorem.paragraph)
+end
+
+Mentor.create(
+  email: "mentor@test.com",
+  password: "123456",
+  first_name: "Monica",
+  last_name: "Mentor",
+  nationality: Faker::Nation.nationality,
+  university: Faker::University.name,
+  major: majors.sample,
+  degree_level: degree_levels.sample,
+  description: Faker::Lorem.paragraph)
+
+puts "Generating fake prospectives ..."
+
+5.times do
+  Prospective.create(
+    email: Faker::Internet.email,
+    password: "123456",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    nationality: Faker::Nation.nationality)
+end
+
+Prospective.create(
+  email: "prospective@test.com",
+  password: "123456",
+  first_name: "Peter",
+  last_name: "Prospective",
+  nationality: Faker::Nation.nationality)
+
+
+
+
