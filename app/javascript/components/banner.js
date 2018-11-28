5204fd9ec0789bc78bd84291daa208d9a1a615be
Thomas Chen <thomas.arsenal.chen@gmail.com>
2018-11-28 15:01:16 +0800

    index page done

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to code"],
+    strings: ["Connect with university students", "Learn more about your majors"],
     typeSpeed: 50,
     loop: true
   });
