5eb5ae0493610b1abb7a10a5f243e1e99507a898
Stuart Nixon <stuartnixon359@gmail.com>
2018-11-28 20:14:03 +0800

    Changed nationality to country and changed some degree levels

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Mentor.delete_all
 
 major_categories = ["Agriculture & Natural Resources","Arts","  Biology & Life Science","Business","Communications & Journalism","Computers & Mathematics","Education","Engineering","Health","Humanities & Liberal Arts","Industrial Arts & Consumer Services","Interdisciplinary","Law & Public Policy","Physical Sciences","Psychology & Social Work","Social Science"]
 majors = ["PLANT SCIENCE AND AGRONOMY", "BIOCHEMICAL SCIENCES", "BUSINESS MANAGEMENT AND ADMINISTRATION", "MATHEMATICS AND COMPUTER SCIENCE", "ARCHITECTURAL ENGINEERING", "PHARMACY PHARMACEUTICAL SCIENCES AND ADMINISTRATION", "ENGLISH LANGUAGE AND LITERATURE", "PUBLIC POLICY", "ECONOMICS", "INTERNATIONAL RELATIONS"]
-degree_levels = ["Undergraduate", "Honours", "Masters", "Doctoral", "Post-doctoral", "Other"]
+degree_levels = ["Diploma", "Associates", "Bachelors", "Honours", "Masters", "Doctoral", "Post-doctoral"]
 
 
 
@@ -24,13 +24,13 @@ puts "Generating fake mentors ..."
     password: "123456",
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    nationality: Faker::Nation.nationality,
+    nationality: Faker::Address.country,
     university: Faker::University.name,
     major_category: major_categories.sample,
     major: majors.sample.titleize,
     degree_level: degree_levels.sample,
     description: Faker::Lorem.paragraph(10, true, 5),
-    description_two: Faker::Lorem.paragraph(5, true, 5))
+    description_two: Faker::Lorem.paragraph(5, true, 3))
 end
 
 Mentor.create(
@@ -38,13 +38,13 @@ Mentor.create(
   password: "123456",
   first_name: "Monica",
   last_name: "Mentor",
-  nationality: Faker::Nation.nationality,
+  nationality: Faker::Address.country,
   university: Faker::University.name,
   major_category: major_categories.sample,
   major: majors.sample.titleize,
   degree_level: degree_levels.sample,
   description: Faker::Lorem.paragraph(10, true, 5),
-  description_two: Faker::Lorem.paragraph(5, true, 5))
+  description_two: Faker::Lorem.paragraph(5, true, 3))
 
 # mentor_start_id = Mentor.last.id - Mentor.count + 1
 # mentor_end_id = Mentor.last.id
@@ -59,7 +59,7 @@ puts "Generating fake prospectives ..."
     password: "123456",
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    nationality: Faker::Nation.nationality)
+    nationality: Faker::Address.country)
 end
 
 Prospective.create(
@@ -67,7 +67,7 @@ Prospective.create(
   password: "123456",
   first_name: "Peter",
   last_name: "Prospective",
-  nationality: Faker::Nation.nationality)
+  nationality: Faker::Address.country)
 
 # prospective_start_id = Prospective.last.id - Prospective.count + 1
 # prospective_end_id = Prospective.last.id
