56d143b2c5bf67c9e175328f54e528222da8884d
Stuart Nixon <stuartnixon359@gmail.com>
2018-11-30 15:32:29 +0800

    Fixed Javascript

--- app/javascript/components/chat.js
+++ app/javascript/components/chat.js
@@ -102,3 +102,5 @@ export class Chat {
   // }
 };
 
+window.Chat = Chat
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,17 +4,9 @@
 import "bootstrap";
 import {Chat} from "components/chat.js"
 
-
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
-
-document.getElementById('startChat').addEventListener("click", () => {
-  // document.getElementById('modalWindow').removeClass('hide');
-  if (document.querySelector(".chat")) {
-      window.chat = new Chat();
-    }
-});
 
